0a7ca3592882f454a2932a9d56c8b2312efe9bee
Lars <l.b.vandenhaak@gmail.com>
2019-01-22 13:43:36 +0100

    Converted to Double, added some testing

--- Gridding.hs
+++ Gridding.hs
@@ -3,7 +3,7 @@
 {-# language ScopedTypeVariables #-}
 {-# language TypeOperators       #-}
 {-# language ViewPatterns        #-}
--- {-# language ScopedTypeVariables #-}
+
 
 module Gridding where
 
@@ -15,17 +15,22 @@ import Data.Array.Accelerate.Interpreter                  as I
 
 import qualified Prelude as P
 import Prelude as P (fromIntegral, fromInteger, fromRational, String, return, (>>=), IO)
+import Data.Char
+
+import Debug.Trace
+import System.IO.Unsafe (unsafePerformIO)
+import Control.Exception
 
 -------------------------------
 -- Gridding Accelerate code
 
 -- # Simple imaging
-simple_imaging :: Exp Float                             -- Field of view size
-               -> Exp Int                               -- Grid size
-               -> Vector (Float, Float, Float)          -- all the uvw baselines (coordinates) (lenght : n * (n-1))
-               -> Vector (Int, Int, Float, Float)       -- NOT USED HERE (Antenna 1, Antenna 2, The time (in MJD UTC), Frequency (Hz)) (lenght : n * (n-1))
-               -> Vector (Complex Float)                -- visibility  (length n * (n-1))
-               -> Acc (Matrix (Complex Float))
+simple_imaging :: Exp Double                             -- Field of view size
+               -> Exp Int                                -- Grid size
+               -> Vector (Double, Double, Double)        -- all the uvw baselines (coordinates) (lenght : n * (n-1))
+               -> Vector (Int, Int, Double, Double)      -- NOT USED HERE (Antenna 1, Antenna 2, The time (in MJD UTC), Frequency (Hz)) (lenght : n * (n-1))
+               -> Vector (Complex Double)                -- visibility  (length n * (n-1))
+               -> Acc (Matrix (Complex Double))
 simple_imaging theta lam uvw src vis =
     let n = round(theta * fromIntegral lam)
         p = map (`div3` fromIntegral lam) (use uvw)
@@ -33,32 +38,32 @@ simple_imaging theta lam uvw src vis =
         guv = fill (index2 n n) czero
     in grid guv p (use vis)
     where
-        div3 :: Exp (Float, Float, Float) -> Exp Float -> Exp (Float, Float, Float)
+        div3 :: Exp (Double, Double, Double) -> Exp Double -> Exp (Double, Double, Double)
         div3 (unlift -> (a,b,c)) x = lift (a / x, b / x, c / x)
 
-grid :: Acc (Matrix (Complex Float))                    -- Destination grid N x N of complex numbers
-     -> Acc (Vector (Float, Float, Float))              -- The uvw baselines, but between -.5 and .5
-     -> Acc (Vector (Complex Float))                    -- The visiblities
-     -> Acc (Matrix (Complex Float))
+grid :: Acc (Matrix (Complex Double))                    -- Destination grid N x N of complex numbers
+     -> Acc (Vector (Double, Double, Double))              -- The uvw baselines, but between -.5 and .5
+     -> Acc (Vector (Complex Double))                    -- The visiblities
+     -> Acc (Matrix (Complex Double))
 grid a p v = permute (+) a (\i -> xy ! i) v
     where
         Z :. n :. _ = unlift (shape a) :: Z :. Exp Int :. Exp Int
         halfn = n `div` 2
-        nf = fromIntegral n :: Exp Float
+        nf = fromIntegral n :: Exp Double
         xy = map gridxy p :: Acc (Vector DIM2)
 
         -- Note the order, python specified as a[y, x] += v
         -- We do the same
-        gridxy :: Exp (Float, Float, Float) -> Exp DIM2
-        gridxy (unlift -> (x, y, _ :: Exp Float)) = index2 (toGridCell y) (toGridCell x)
+        gridxy :: Exp (Double, Double, Double) -> Exp DIM2
+        gridxy (unlift -> (x, y, _ :: Exp Double)) = index2 (toGridCell y) (toGridCell x)
 
-        toGridCell :: Exp Float -> Exp Int
+        toGridCell :: Exp Double -> Exp Int
         toGridCell f = halfn + floor (0.5 + nf * f)
 
 -- # Normal imaging
 frac_coord :: Exp Int                                   -- The height/width
            -> Exp Int                                   -- Oversampling factor
-           -> Acc (Vector Float)                        -- a uvw baseline, but between -.5 and .5
+           -> Acc (Vector Double)                        -- a uvw baseline, but between -.5 and .5
            -> Acc (Vector (Int, Int))
 frac_coord n qpx p = 
     let halfn = n `div` 2
@@ -74,7 +79,7 @@ frac_coord n qpx p =
 
 frac_coords :: Exp (Int, Int)                           -- (Heigt, width) of the grid
             -> Exp Int                                  -- Oversampling factor
-            -> Acc (Vector (Float, Float, Float))       -- The uvw baselines, but between -.5 and .5
+            -> Acc (Vector (Double, Double, Double))       -- The uvw baselines, but between -.5 and .5
             -> Acc (Vector (Int, Int, Int, Int))
 -- NOTE we should give it in height, width order.
 frac_coords (unlift -> (h, w) ) qpx p =
@@ -83,11 +88,11 @@ frac_coords (unlift -> (h, w) ) qpx p =
         (y, yf)   = unzip (frac_coord h qpx v)
     in zip4 x xf y yf
 
-convgrid :: Array DIM4 (Complex Float)                  -- The oversampled convolution kernel
-         -> Acc (Matrix (Complex Float))                -- Destination grid N x N of complex numbers
-         -> Acc (Vector (Float, Float, Float))          -- The uvw baselines, but between -.5 and .5
-         -> Acc (Vector (Complex Float))                -- The visiblities
-         -> Acc (Matrix (Complex Float))
+convgrid :: Array DIM4 (Complex Double)                  -- The oversampled convolution kernel
+         -> Acc (Matrix (Complex Double))                -- Destination grid N x N of complex numbers
+         -> Acc (Vector (Double, Double, Double))          -- The uvw baselines, but between -.5 and .5
+         -> Acc (Vector (Complex Double))                -- The visiblities
+         -> Acc (Matrix (Complex Double))
 convgrid gcf a p v =
     let Z :. qpx :. _ :. gh :. gw = (arrayShape gcf) :: Z :. Int :. Int :. Int :. Int
         Z :. height :. width = unlift (shape a) :: Z :. Exp Int :. Exp Int
@@ -96,12 +101,12 @@ convgrid gcf a p v =
         halfgh = constant $ gh `div` 2
         halfgw = constant $ gw `div` 2
 
-        getComplex :: Int -> Int -> Exp (Int, Int, Int, Int) -> Exp (Int, Int, Complex Float)
+        getComplex :: Int -> Int -> Exp (Int, Int, Int, Int) -> Exp (Int, Int, Complex Double)
         getComplex i j (unlift -> (x, xf, y, yf)::(Exp Int,Exp Int,Exp Int,Exp Int)) =
             lift ( x
                  , y
                  , gpugcf ! lift (Z :. yf :. xf :. constant i :. constant j) )
-        fullrepli :: Int -> Int -> Acc (Matrix (Complex Float)) -> Acc (Matrix (Complex Float))
+        fullrepli :: Int -> Int -> Acc (Matrix (Complex Double)) -> Acc (Matrix (Complex Double))
         fullrepli i j origin = 
             let temp = map (getComplex i j) coords
                 (x, y, source) = unzip3 temp
@@ -114,6 +119,65 @@ convgrid gcf a p v =
 myfor :: Int -> (Int -> a -> a) -> a -> a
 myfor n f x | n P.== 0  = x
             | P.otherwise =  myfor (n-1) f (f (n-1) x)
+
+----------------------
+-- Testing stuff
+testData :: ([Complex Double], [(Double, Double, Double)])
+testData = unsafePerformIO testData0
+
+testSimple :: IO ()
+testSimple = do
+    testData <- testData0
+    let n   = (P.length . P.fst) testData
+        m   = (P.length . P.snd) testData
+        size = assert (n P.== m) $ Z :. n
+        vis = fromList size $ P.fst testData
+        uvw = fromList size $ P.snd testData
+        src      = undefined
+        theta    = 2*0.05
+        lam      = 18000
+        result = simple_imaging theta lam uvw src vis
+        runresult = CPU.run result
+        fst = indexArray runresult (Z :. 0 :. 0)
+    P.putStrLn (P.show fst)
+
+
+
+
+
+
+-------------------------
+-- Test input reading
+testData0 :: IO ([Complex Double], [(Double, Double, Double)])
+testData0 = do
+    visData <- P.readFile "vis.csv"
+    uvwData <- P.readFile "uvw.csv"
+    let vislines = P.lines visData
+        visdat   = P.map parseComplex vislines
+        uvwlines = P.lines uvwData
+        uvwdat   = P.map parseTuple3 uvwlines
+    return (visdat, uvwdat)
+
+parseComplex :: String -> Complex Double
+parseComplex s  =
+    case P.head s of
+        ' ' -> parseComplex (P.tail s)
+        '(' -> parseComplex (P.tail s)
+        _   -> let (x, rest) = readDouble s
+                   (y, _)    = readDouble rest
+               in P.read x :+ P.read y
+    where
+        readDouble s@('-':_) = P.splitAt 25 s
+        readDouble ('+':s)   = P.splitAt 24 s
+        readDouble s         = P.splitAt 24 s
+
+parseTuple3 :: P.Read a => String -> (a,a,a)
+parseTuple3 s = 
+    let valid c = c P./= ',' P.&& P.not (isSpace c)
+        (x, _ : rest0) = P.span valid s
+        (y, _ : rest1) = P.span valid rest0
+        (z, _)         = P.span valid rest1
+    in (P.read x, P.read y, P.read z)
 
 testing :: Acc (Vector Int)
 testing = use $ fromList (Z :. 5) [0..]
@@ -139,11 +203,11 @@ def convgrid(
     -- The oversampled convolution kernel (most of the time it is shaped like 2 x 2 x 31 x 31)
     gcf, :: 
     --Destination grid N x N of complex numbers
-    a,  :: Matrix (Complex Float)
+    a,  :: Matrix (Complex Double)
     -- The uvw baselines, but between -.5 and .5
-    p,  :: [(Float, Float, Float)]
+    p,  :: [(Double, Double, Double)]
     -- The visiblities
-    v :: [Complex Float]
+    v :: [Complex Double]
     ):
     """Grid after convolving with gcf
 
@@ -168,15 +232,15 @@ def convgrid(
 {-
 def simple_imaging(
     --Field of view size
-    theta, :: Float
+    theta, :: Double
     --Grid size
     lam,  :: Int
     -- all the uvw baselines (coordinates) (lenght : n * (n-1))                          
-    uvw,  :: [(Float, Float, Float)]
+    uvw,  :: [(Double, Double, Double)]
     -- (Antenna 1, Antenna 2, The time (in MJD UTC), Frequency (Hz)) (lenght : n * (n-1))                     
-    src,  :: [(Int, Int, Float, Float)] 
+    src,  :: [(Int, Int, Double, Double)] 
     -- The visibility 
-    vis :: [Complex Float]
+    vis :: [Complex Double]
     ):
     """Trivial function for imaging
     Does no convolution but simply puts the visibilities into a grid
@@ -192,11 +256,11 @@ return guv
 
 def grid(
     --Destination grid N x N of complex numbers
-    a, :: Matrix (Complex Float)
+    a, :: Matrix (Complex Double)
     -- The uvw baselines, but between -.5 and .5
-    p, :: [(Float, Float, Float)]
+    p, :: [(Double, Double, Double)]
     -- The visiblities
-    v :: [Complex Float]
+    v :: [Complex Double]
     ):
     """Grid visibilities (v) at positions (p) into (a) without convolution
     :param a:   The uv plane to grid to (updated in-place!)
@@ -216,7 +280,7 @@ def frac_coord(
     -- Oversampling factor
     Qpx, :: Int
     -- a uvw baseline, but between -.5 and .5
-    p, :: [Float]
+    p, :: [Double]
     ):
     """
     Compute whole and fractional parts of coordinates, rounded to
@@ -253,11 +317,11 @@ def convgrid(
     -- The oversampled convolution kernel (most of the time it is shaped like 2 x 2 x 31 x 31)
     gcf, :: 
     --Destination grid N x N of complex numbers
-    a,  :: Matrix (Complex Float)
+    a,  :: Matrix (Complex Double)
     -- The uvw baselines, but between -.5 and .5
-    p,  :: [(Float, Float, Float)]
+    p,  :: [(Double, Double, Double)]
     -- The visiblities
-    v :: [Complex Float]
+    v :: [Complex Double]
     ):
     """Grid after convolving with gcf
 
