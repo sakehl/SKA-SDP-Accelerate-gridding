786d253dde37ad41621a5c8427383140a12cf35a
Lars <l.b.vandenhaak@gmail.com>
2019-04-01 11:53:07 +0200

    Added better argument parser

--- GPU/GPUMain.hs
+++ GPU/GPUMain.hs
@@ -11,24 +11,22 @@ import Data.Array.Accelerate.LLVM.PTX                     as GPU
 
 import Data.Array.Accelerate.Debug                        as A
 
+data Args = Args { n   :: Maybe Int
+                 , gpu :: Bool}
+
+defArgs = Args (Just 500) False
+
 main :: IO ()
 main = do 
     remover "result.h5"
     --setFlag dump_phases
     args <- getArgs
-    let n = case args of
-                [] -> Just 500
-                "all":_ -> Nothing
-                x:_ -> Just $ read x
-    let isGPU = case args of
-        [] -> False
-        [_:"GPU":_] -> True
-        [_:"Gpu":_] -> True
-        [_:"gpu":_] -> True
-        _           -> False
+    let parsedArgs = parser defArgs args
+        n_         = n parsedArgs
+        isGPU      = gpu parsedArgs
     fourier <- if isGPU 
-                then aw_gridding GPU.run GPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n
-                else aw_gridding CPU.run CPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n
+                then aw_gridding GPU.run GPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n_
+                else aw_gridding CPU.run CPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n_
     putStrLn (show fourier)
 
 
@@ -37,3 +35,14 @@ remover fileName = removeFile fileName `catch` handle
     where handle e
             | isDoesNotExistError e = return ()
             | otherwise = throwIO e
+
+parser :: Args -> [String] -> Args
+parser args []          = args
+parser args ("-g":xs)   = parser args{gpu = True} xs
+parser args ("-G":xs)   = parser args{gpu = True} xs
+parser args ("-gpu":xs) = parser args{gpu = True} xs
+parser args ("-GPU":xs) = parser args{gpu = True} xs
+parser args ("-Gpu":xs) = parser args{gpu = True} xs
+parser args ("-n":n:xs) = parser args{n = Just $ read n} xs
+parser args ("-all":xs) = parser args{n = Nothing} xs
+parser args xs          = error $ "Error while parsing" ++ show xs
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-    
+
 import ImageDataset
 import System.Directory
 import Control.Exception
@@ -7,15 +7,18 @@ import System.IO.Error hiding (catch)
 import System.Environment
 import Data.Array.Accelerate.LLVM.Native                  as CPU
 
+data Args = Args { n   :: Maybe Int
+                 , gpu :: Bool}
+
+defArgs = Args (Just 500) False
+
 main :: IO ()
-main = do 
+main = do
     remover "result.h5"
     args <- getArgs
-    let n = case args of
-                [] -> Just 500
-                "all":_ -> Nothing
-                x:_ -> Just $ read x
-    fourier <- aw_gridding CPU.run CPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n 
+    let parsedArgs = parser defArgs args
+        n_          = n parsedArgs
+    fourier <- aw_gridding CPU.run CPU.runN "data/SKA1_Low_wkern2.h5" "data/SKA1_Low_akern3.h5" "data/SKA1_Low_quick.h5" n_
     putStrLn (show fourier)
 
 
@@ -24,3 +27,14 @@ remover fileName = removeFile fileName `catch` handle
     where handle e
             | isDoesNotExistError e = return ()
             | otherwise = throwIO e
+
+parser :: Args -> [String] -> Args
+parser args []          = args
+parser args ("-g":xs)   = parser args{gpu = True} xs
+parser args ("-G":xs)   = parser args{gpu = True} xs
+parser args ("-gpu":xs) = parser args{gpu = True} xs
+parser args ("-GPU":xs) = parser args{gpu = True} xs
+parser args ("-Gpu":xs) = parser args{gpu = True} xs
+parser args ("-n":n:xs) = parser args{n = Just $ read n} xs
+parser args ("-all":xs) = parser args{n = Nothing} xs
+parser args xs          = error $ "Error while parsing" ++ show xs
