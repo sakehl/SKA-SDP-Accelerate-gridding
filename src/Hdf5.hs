e43da41f1fb01252d399fd69388995e0ac41992d
Lars <l.b.vandenhaak@gmail.com>
2019-04-05 16:39:00 +0200

    Added thehdf5 functions to work with old acelerate

--- src/Hdf5.hs
+++ src/Hdf5.hs
@@ -39,16 +39,16 @@ foreign import ccall unsafe "readDatasetInt"
     c_readDatasetInt :: CString -> CString -> Ptr CLong -> IO ()
 
 foreign import ccall unsafe "readDatasetLLong"
-    c_readDatasetInt64 :: CString -> CString -> Ptr CLong -> IO ()
+    c_readDatasetInt64 :: CString -> CString -> Ptr Int64 -> IO ()
 
 foreign import ccall unsafe "readDatasetDouble"
-    c_readDatasetDouble :: CString -> CString -> Ptr CDouble -> IO ()
+    c_readDatasetDouble :: CString -> CString -> Ptr Double -> IO ()
 
 foreign import ccall unsafe "readDatasetComplex"
-    c_readDatasetComplex :: CString -> CString -> Ptr ComplexCDouble -> IO ()
+    c_readDatasetComplex :: CString -> CString -> Ptr ComplexDouble -> IO ()
 
 foreign import ccall unsafe "readDatasetsComplex"
-    c_readDatasetsComplex :: CString -> Ptr CString -> Ptr ComplexCDouble -> IO ()
+    c_readDatasetsComplex :: CString -> Ptr CString -> Ptr ComplexDouble -> IO ()
 
 foreign import ccall unsafe "readDatasetsDouble"
     c_readDatasetsDouble :: CString -> Ptr CString -> Ptr CDouble -> IO ()
@@ -81,6 +81,57 @@ createDatasetV c_createDataset name' dataset' rank dims dat = do
     name <- newCString name'
     dataset <- newCString dataset'
     SV.unsafeWith dims $ \dimsp -> SV.unsafeWith dat $ \datap -> c_createDataset name dataset rank dimsp datap
+
+readDatasetsComplexV :: String -> [String] -> IO (SV.Vector ComplexDouble)
+readDatasetsComplexV = readDatasetsV c_readDatasetsComplex
+
+readDatasetInt64V :: String -> String -> IO (SV.Vector Int64)
+readDatasetInt64V = readDatasetV c_readDatasetInt64
+readDatasetDoubleV :: String -> String -> IO (SV.Vector Double)
+readDatasetDoubleV = readDatasetV c_readDatasetDouble
+readDatasetComplexV :: String -> String -> IO (SV.Vector ComplexDouble)
+readDatasetComplexV = readDatasetV c_readDatasetComplex
+
+    
+readDatasetV :: Storable a => (CString -> CString -> Ptr a -> IO ()) 
+    -> String -> String -> IO (SV.Vector a)
+readDatasetV reader name' dataset' = do
+    name <- newCString name'
+    dataset <- newCString dataset'
+    --Get the rank and dimensions, and calculate how many elements the dataset has
+    rank <- getRankDataset name' dataset'
+    dims <- getDimsDataset name' dataset' rank
+    let total = SV.product dims
+    -- Allocate enough memory for the array
+    dat <- mallocArray (fromIntegral total)
+    -- Get the data via the FFI binding
+    reader name dataset dat
+    -- Make it to a foreign pointer, this will make sure it gets freed eventually and we can get a Vector out of it
+    foreign_dat <- newForeignPtr finalizerFree dat
+    -- Give us back a vector :)
+    return $ SV.unsafeFromForeignPtr0 foreign_dat (fromIntegral total)
+
+-- We will assume for efficient code, that each dataset has exactly the same type and dimenions.
+readDatasetsV :: Storable a => (CString -> Ptr CString -> Ptr a -> IO ()) 
+    -> String -> [String] -> IO (SV.Vector a)
+readDatasetsV reader name' datasets' = do
+    datasets <- mapM newCString datasets'
+    datasetsPtr <- newArray0 nullPtr datasets
+    name <- newCString name'
+    --dataset <- newCString dataset'
+    --Get the rank and dimensions, and calculate how many elements the dataset has
+    rank <- getRankDataset name' (head datasets')
+    dims <- getDimsDataset name' (head datasets') rank
+    let total = length datasets' * fromIntegral (SV.product dims)
+    -- Allocate enough memory for the array
+    dat <- mallocArray total
+    -- Get the data via the FFI binding
+    reader name datasetsPtr dat
+    -- Make it to a foreign pointer, this will make sure it gets freed eventually and we can get a Vector out of it
+    foreign_dat <- newForeignPtr finalizerFree dat
+    -- Give us back a vector :)
+    return $ SV.unsafeFromForeignPtr0 foreign_dat (fromIntegral total)
+
 
 getDimsDataset :: String -> String -> CInt -> IO (SV.Vector CInt)
 getDimsDataset name' dataset' rank = do
@@ -191,17 +242,56 @@ readDatasetsComplex n ds = do
     return $ unsafeCastDataSet m
     -}
 
-readDatasetComplex :: (A.Shape sh) => String -> String -> IO (A.Array sh ComplexDouble)
-readDatasetComplex n1 n2 = undefined
+readDatasetComplex ::  forall sh . A.Shape sh => String -> String -> IO (A.Array sh ComplexDouble)
+readDatasetComplex name dataset = do
+    vector <- readDatasetComplexV name dataset
+    rank <- getRankDataset name dataset
+    dims <- getDimsDataset name dataset rank
+    let askedRank = A.arrayRank . A.arrayShape $ (undefined :: A.Array sh ComplexDouble)
+        shape = assert (if (askedRank == fromIntegral rank) then True else error (printf "Assertion failed for dataset %s in file %s. Expected rank: %i, actual rank: %i" dataset name askedRank (fromIntegral rank :: Int)))
+                . A.listToShape . reverse . SV.toList . SV.map fromIntegral $ dims
+    let vector2 = (((), SV.map myreal vector), SV.map myimag vector)
+    return $ A.fromVectors shape vector2
+        where
+            myreal (x :+ _) = x
+            myimag (_ :+ y) = y
 
-readDatasetInt64 :: (A.Shape sh) => String -> String -> IO (A.Array sh Int64)
-readDatasetInt64 n1 n2 = undefined
+readDatasetInt64 ::  forall sh . A.Shape sh => String -> String -> IO (A.Array sh Int64)
+readDatasetInt64 name dataset = do
+    vector <- readDatasetInt64V name dataset
+    --let vectorD = SV.unsafeCast vector
+    rank <- getRankDataset name dataset
+    dims <- getDimsDataset name dataset rank
+    let askedRank = A.arrayRank . A.arrayShape $ (undefined :: A.Array sh Int64)
+        shape = assert (if (askedRank == fromIntegral rank) then True else error (printf "Assertion failed for dataset %s in file %s. Expected rank: %i, actual rank: %i" dataset name askedRank (fromIntegral rank :: Int)))
+                . A.listToShape . reverse . SV.toList . SV.map fromIntegral $ dims
+    return $ A.fromVectors shape vector
 
-readDatasetDouble :: (A.Shape sh) => String -> String -> IO (A.Array sh Double)
-readDatasetDouble n1 n2 = undefined
+readDatasetDouble ::  forall sh . A.Shape sh => String -> String -> IO (A.Array sh Double)
+readDatasetDouble name dataset = do
+    vector <- readDatasetDoubleV name dataset
+    --let vectorD = SV.unsafeCast vector
+    rank <- getRankDataset name dataset
+    dims <- getDimsDataset name dataset rank
+    let askedRank = A.arrayRank . A.arrayShape $ (undefined :: A.Array sh Double)
+        shape = assert (if (askedRank == fromIntegral rank) then True else error (printf "Assertion failed for dataset %s in file %s. Expected rank: %i, actual rank: %i" dataset name askedRank (fromIntegral rank :: Int)))
+                . A.listToShape . reverse . SV.toList . SV.map fromIntegral $ dims
+    return $ A.fromVectors shape vector
 
-readDatasetsComplex :: (A.Shape sh) => String -> [String] -> IO (A.Array sh ComplexDouble)
-readDatasetsComplex n ds = undefined
+readDatasetsComplex ::  forall sh . A.Shape sh => String -> [String] -> IO (A.Array sh ComplexDouble)
+readDatasetsComplex name datasets = do
+    vector <- readDatasetsComplexV name datasets
+    rank <- getRankDataset name (head datasets)
+    dims <- getDimsDataset name (head datasets) rank
+    let n = length datasets
+        askedRank = A.arrayRank . A.arrayShape $ (undefined :: A.Array sh ComplexDouble)
+        shape = assert (if (askedRank == fromIntegral rank + 1) then True else error (printf "Assertion failed for dataset %s in file %s. Expected rank: %i, actual rank: %i" (head datasets) name askedRank (fromIntegral rank :: Int)))
+                . A.listToShape . reverse . (n:) . SV.toList . SV.map fromIntegral $ dims
+    let vector2 = (((), SV.map myreal vector), SV.map myimag vector)
+    return $ A.fromVectors shape vector2
+        where
+            myreal (x :+ _) = x
+            myimag (_ :+ y) = y
 
 createDatasetDouble :: (A.Shape sh) => String -> String -> A.Array sh Double -> IO ()
 createDatasetDouble name dataset dat= do
